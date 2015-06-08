6df83ca743408c3d0a6c342a1f6139ffb85d2541
Zlatozar Zhelyazkov <zlatozar@gmail.com>
2015-06-08 16:42:31 +0300

    Add helpful macros
    Signed-off-by: Zlatozar Zhelyazkov <zlatozar@gmail.com>

--- auxfns.lisp
+++ auxfns.lisp
@@ -138,6 +138,44 @@ i.e. (compose f g h) = (lambda (x) (f (g (h x))))."
   (and (consp x) (null (cdr x))))
 
 ;;; ____________________________________________________________________________
+;;;                                                                      Macros
+
+(defmacro once-only (variables &rest body)
+  "Returns the code built by BODY.  If any of VARIABLES
+  might have side effects, they are evaluated once and stored
+  in temporary variables that are then passed to BODY."
+  (assert (every #'symbolp variables))
+  (let ((temps nil))
+    (dotimes (i (length variables)) (push (gensym) temps))
+    `(if (every #'side-effect-free? (list .,variables))
+         (progn .,body)
+         (list 'let
+               ,`(list ,@(mapcar #'(lambda (tmp var)
+                                     `(list ',tmp ,var))
+                                 temps variables))
+               (let ,(mapcar #'(lambda (var tmp) `(,var ',tmp))
+                             variables temps)
+                 .,body)))))
+
+(defun side-effect-free? (exp)
+  "Is exp a constant, variable, or function,
+  or of the form (THE type x) where x is side-effect-free?"
+  (or (atom exp) (constantp exp)
+      (starts-with exp 'function)
+      (and (starts-with exp 'the)
+           (side-effect-free? (third exp)))))
+
+(defmacro funcall-if (fn arg)
+  (once-only (fn)
+             `(if ,fn (funcall ,fn ,arg) ,arg)))
+
+(defmacro read-time-case (first-case &rest other-cases)
+  "Do the first case, where normally cases are
+  specified with #+ or possibly #- marks."
+  (declare (ignore other-cases))
+  first-case)
+
+;;; ____________________________________________________________________________
 
 (defun rest2 (x)
   "The rest of a list after the first two elements."
